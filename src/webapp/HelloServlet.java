9872993b80a21b14b0f74a81c7f6f178e20e4bb0
Keita Takagi <97935891+ttc-takagi@users.noreply.github.com>
2022-02-21 13:37:17 +0900

    Update HelloServlet.java

--- src/webapp/HelloServlet.java
+++ src/webapp/HelloServlet.java
@@ -11,6 +11,6 @@ public class HelloServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.getWriter().write("hello Jenkins2");
+		resp.getWriter().write("AWS実習課題");
 	}
 }
