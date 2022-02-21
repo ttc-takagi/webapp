d044221680ae4ccb4ac383f5bb84fb036d03a825
Keita Takagi <97935891+ttc-takagi@users.noreply.github.com>
2022-02-21 13:45:26 +0900

    Update HelloServlet.java

--- src/webapp/HelloServlet.java
+++ src/webapp/HelloServlet.java
@@ -11,6 +11,7 @@ public class HelloServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setCharacterEncoding("UTF-8");
 		resp.getWriter().write("AWS実習課題");
 	}
 }
